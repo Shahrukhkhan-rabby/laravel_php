6831e1d309cdebe555ffb5db1c9c25ba64f25ec4
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-04-06 14:34:36 +0600

    Fix user profile page: Corrected user retrieval in controller and fixed username display in view

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($user)
     {
-        return view('home');
+        $user = User::find($user);
+        if(!$user){
+            abort(404, "User not found");
+        }
+
+        return view('home', [
+            'user' => $user,
+
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row">
         <div class="col-3 p-5"><img src="/logo/peaceit.jpeg" style="height: 200px; width: 200px; border-radius: 50%"></div>
         <div class="col-9 pt-5">
-            <div><h1>PeaceIT</h1></div>
+            <div>
+                <h1>{{ $user->username }}</h1>
+            </div>
             <div class="d-flex">
                 <div class="pe-5"><strong>153</strong> posts</div>
                 <div class="pe-5"><strong>23k</strong> followers</div>
